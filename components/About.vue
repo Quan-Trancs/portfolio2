e0db91c946ef9c3b6d86e680714bbf4449242ca3
QuanTA18 <unconfigured@null.spigotmc.org>
2024-01-01 22:46:15 -0700

    config about me section body article [Quan Tran]

--- components/About.vue
+++ components/About.vue
@@ -6,7 +6,7 @@
                 <div class="mt-5 text-5xl font-bold text-center">About Me</div>
             </div>
             <div class="w-full flex  justify-center">
-                <div class="w-[900px] text-center text-2xl">I am Quan Tran, a Computer Science student at the Arizona State University. My passion lies where technology, creativity, and coding intersect. With two years of coding experience, I've explored front-end development through projects, internships, and classes. I also paraticipated in challenging hackathons and competitions. My portfolio showcases my experiences and skills in this exciting industry. Come along as we explore the realm of coding and design.</div>
+                <div class="w-[900px] text-center text-2xl">I am Quan Tran, a Computer Science student at the Arizona State University. My passion lies btween technology, creativity, and coding. With two years of coding experience, I've explored full-stack development through projects, internships, and classes. I also paraticipated in challenging hackathons and competitions. My portfolio showcases my experiences and skills in this exciting industry. Come along as we explore the realm of Software Developing.</div>
             </div>
         </div>
     </div>
